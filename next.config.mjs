4f0b4b9dc1a8d362237b34c40ced46f270d73e20
Sangheon Lee <wwdbsh@gmail.com>
2024-08-14 01:49:33 -0700

    next cache 비활성화

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)\\.css",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
